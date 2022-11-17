377a5104efdd958d9946b0b310cf872d1efc595b
Mercy <mercyomole229@gmail.com>
2022-11-16 17:40:10 -0800

    builtin1

--- builtin1.c
+++ builtin1.c
@@ -3,7 +3,7 @@
 /**
  * _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. It Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
