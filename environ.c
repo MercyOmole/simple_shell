9b23b4441caadd48c27188ba866764e16315fccc
Mercy <mercyomole229@gmail.com>
2022-11-16 17:42:32 -0800

    prints new environment

--- environ.c
+++ environ.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _myenv - prints the current environment in shell.
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
