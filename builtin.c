62fe40c8cdd605d75ec6485dd7f0239792cd3b04
Mercy <mercyomole229@gmail.com>
2022-11-16 17:38:22 -0800

    Exits the shell

--- builtin.c
+++ builtin.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
+ * _myexit - exits the shell.
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: exits with a given exit status
