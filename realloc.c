a0eb9a97677d6da9e2ff181e70ad41a790fb09e1
Mercy <mercyomole229@gmail.com>
2022-11-16 18:16:16 -0800

    Reallocates

--- realloc.c
+++ realloc.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
+ **_memset - fills memory with a constant byte.
  *@s: the pointer to the memory area
  *@b: the byte to fill *s with
  *@n: the amount of bytes to be filled
